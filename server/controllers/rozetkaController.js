3847e00654663601fc872d0a855c58b5da6dc1ed
Professional <qqalopruvet@gmail.com>
2025-10-16 02:12:14 +0300

    download products

--- server/controllers/rozetkaController.js
+++ server/controllers/rozetkaController.js
@@ -584,7 +584,7 @@ class RozetkaController {
                             const value = await FilterValues.findOne({where: {name: paramCode}})
                             if (value) {
                                 console.log({product_id: device.id, filter_value_id: value.id})
-                                await FilterProductValue.create({product_id: device.id, filter_value_id: value.id})
+                                await FilterProductValue.findOrCreate({product_id: device.id, filter_value_id: value.id})
                             } else {
                                 const filter = await Filters.findOne({where: {name: paramName}})
                                 if (filter) {
@@ -593,7 +593,7 @@ class RozetkaController {
                                         code: await Transliterations(paramCode),
                                         filter_id: filter.id
                                     })
-                                    await FilterProductValue.create({product_id: device.id, filter_value_id: value.id})
+                                    await FilterProductValue.findOrCreate({product_id: device.id, filter_value_id: value.id})
                                 } else {
                                     const filter = await Filters.create({
                                         name: paramName,
@@ -604,7 +604,7 @@ class RozetkaController {
                                         code: await Transliterations(paramCode),
                                         filter_id: filter.id
                                     })
-                                    await FilterProductValue.create({product_id: device.id, filter_value_id: value.id})
+                                    await FilterProductValue.findOrCreate({product_id: device.id, filter_value_id: value.id})
                                 }
                             }
                         }
