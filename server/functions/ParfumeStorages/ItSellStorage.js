420178a3b20d640cd1615d506c61fc9e3beeb8cf
Professional <qqalopruvet@gmail.com>
2025-11-29 21:45:18 +0200

    search

--- server/functions/ParfumeStorages/ItSellStorage.js
+++ server/functions/ParfumeStorages/ItSellStorage.js
@@ -12,7 +12,15 @@ async function ItSellStorage() {
         const page = await axios.get('https://itsellopt.ua/price_list', {
             responseType: 'text',
             maxRedirects: 0,
-            validateStatus: (status) => status >= 200 && status < 400
+            validateStatus: (status) => status >= 200 && status < 400,
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept-Language': 'uk-UA,uk;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Connection': 'keep-alive',
+                'Upgrade-Insecure-Requests': '1'
+            }
         })
         const redirectUrl = page.headers.location;
         console.log('Redirect URL:', redirectUrl);
