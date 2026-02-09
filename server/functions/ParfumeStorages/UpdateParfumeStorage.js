8537d450b421d88afc50a190b624b5121f228c0b
Professional <qqalopruvet@gmail.com>
2026-02-09 20:34:30 +0200

    search

--- server/functions/ParfumeStorages/UpdateParfumeStorage.js
+++ server/functions/ParfumeStorages/UpdateParfumeStorage.js
@@ -72,7 +72,9 @@ async function UpdateParfumeStorage() {
                             const cheepItemPrice = calcStartPrice < 249 ? calcStartPrice + 40 : calcStartPrice;
                             calcPrice = cheepItemPrice < 2000 ? cheepItemPrice * 1.13 : cheepItemPrice * 1.1
                         }else if(value.list === 'it') {
-                            const diff =  +value.sell_price * 0.85 - calcStartPrice
+
+                            const sellPrice = value.sell_price * 0.85
+                            const diff =  sellPrice - calcStartPrice
                             if(calcStartPrice < 249) {
                                 calcPrice = diff < 60 ? calcStartPrice + 60 : +value.sell_price * 0.85
                             }else if(calcStartPrice > 2000){
@@ -80,7 +82,11 @@ async function UpdateParfumeStorage() {
                             }else{
                                 calcPrice = diff < 80 ? calcStartPrice + 80 : +value.sell_price * 0.85
                             }
-
+                            if(value.code === 'it-00000076774_3'){
+                                console.log({
+                                    calcPrice,calcStartPrice,price:value.price,sell_price:value.sell_price
+                                })
+                            }
                         }
                         storageType = optionStorageType;
                     }
